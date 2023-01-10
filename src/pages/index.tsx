b3cfbf8dd1ea6b211a4d7388264f657af53b9969
Vishal Sathyanarayana <vishal23dps@gmail.com>
2023-01-10 15:44:15 -0700

    killed example once and for all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,11 +68,6 @@ export default Home;
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
-  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined }
-  // );
-
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
