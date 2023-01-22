43f58ee85b2a263480df303bfb8fa64676b40e7d
Vishal Sathyanarayana <vishal23dps@gmail.com>
2023-01-21 19:32:37 -0700

    fixed apostrophe thing

--- src/utils/api-helpers.ts
+++ src/utils/api-helpers.ts
@@ -10,7 +10,7 @@ export async function fetchGetJSON(url: string) {
   }
 }
 
-export async function fetchPostJSON(url: string, data?: {}) {
+export async function fetchPostJSON(url: string, data?: object) {
   try {
     // Default options are marked with *
     const response = await fetch(url, {
