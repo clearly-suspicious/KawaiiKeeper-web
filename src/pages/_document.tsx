da962581097b8dac3590a54515917b8331d66255
clearly-outsane <1starwarrior516@gmail.com>
2023-03-05 16:41:36 +0530

    radix adding relative to body override

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body style={{ position: "initial !important" as "initial" }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
